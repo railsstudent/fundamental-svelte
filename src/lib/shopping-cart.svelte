504b0bc633ca8b72b992a3a0df6bb9fc89ea6461
Connie Leung <cleung334@gmail.com>
2025-05-06 23:17:41 +0800

    refactor: move to ShoppingCart component

---
+++ src/lib/shopping-cart.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    let header = $state('Shopping List App');
+
+    let items = $state<{ id: number, label: string }[]>([
+        {
+            id: 1,
+            label: '10 Apples'
+        },
+        {
+            id: 2,
+            label: '20 Bananas'
+        },
+        {
+            id: 3,
+            label: '5 Oranges'
+        },
+        {
+            id: 4,
+            label: '15 Strawberries'
+        }
+    ]);
+</script>
+
+<h1>{ header }</h1>
+<ul>
+    {#each items as item (item.id)}
+        <li>{ item.id } - { item.label }</li>
+    {/each}
+</ul>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,29 +1,8 @@
 <script lang="ts">
-    let header = $state('Shopping List App');
-
-    let items = $state<{ id: number, label: string }[]>([
-        {
-            id: 1,
-            label: '10 Apples'
-        },
-        {
-            id: 2,
-            label: '20 Bananas'
-        },
-        {
-            id: 3,
-            label: '5 Oranges'
-        }
-    ]);
+    import ShoppingCart from '$lib/shopping-cart.svelte';
 </script>
 
-<h1>{ header }</h1>
-<ul>
-    {#each items as item (item.id)}
-        <li>{ item.id } - { item.label }</li>
-    {/each}
-</ul>
-
+<ShoppingCart />
 <style>
     :global {
         body {
