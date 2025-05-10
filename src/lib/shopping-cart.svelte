4e81021234d17ee4358e4e05d9866293bcdf6a8a
Connie Leung <cleung334@gmail.com>
2025-05-10 08:04:20 +0800

    feat: complete day 5 exercise.  use the control flow to show/hide UI.

--- src/lib/shopping-cart.svelte
+++ src/lib/shopping-cart.svelte
@@ -2,25 +2,8 @@
 	let header = $state('Shopping List App');
 	let newItem = $state('');
 	let newItemHigerPriority = $state(false);
-
-	let items = $state([
-		{
-			id: 1,
-			label: '10 Apples'
-		},
-		{
-			id: 2,
-			label: '20 Bananas'
-		},
-		{
-			id: 3,
-			label: '5 Oranges'
-		},
-		{
-			id: 4,
-			label: '15 Strawberries'
-		}
-	]);
+	let items = $state([] as { id: number; label: string }[]);
+	let isEditing = $state(false);
 
 	function saveItem() {
 		if (newItem) {
@@ -29,8 +12,6 @@
 				label: newItem
 			});
 			newItem = '';
-
-			console.log('items in saveItem', items);
 		}
 	}
 
@@ -38,23 +19,43 @@
 		event.preventDefault();
 		saveItem();
 	}
+
+	function toggleEdit(value: boolean) {
+		isEditing = value;
+		newItem = '';
+	}
 </script>
 
-<h1>{header}</h1>
-<ul>
-	{#each items as item (item.id)}
-		<li>{item.id} - {item.label}</li>
-	{/each}
-</ul>
-<form class="add-item-form" method="POST" onsubmit={handleSubmit}>
-	<input id="newItem" name="newItem" type="text" placeholder="Add item" bind:value={newItem} />
-	<label>
-		<input
-			id="higherPriority"
-			name="higherPriority"
-			type="checkbox"
-			bind:checked={newItemHigerPriority}
-		/>Higher Priority
-	</label>
-	<button class="btn btn-primary">Save Item</button>
-</form>
+<div class="header">
+	<h1>{header}</h1>
+	{#if isEditing}
+		<button class="btn" onclick={() => toggleEdit(false)}>Cancel</button>
+	{:else}
+		<button class="btn btn-primary" onclick={() => toggleEdit(true)}>Add Item</button>
+	{/if}
+</div>
+
+{#if isEditing}
+	<form class="add-item-form" method="POST" onsubmit={handleSubmit}>
+		<input id="newItem" name="newItem" type="text" placeholder="Add item" bind:value={newItem} />
+		<label>
+			<input
+				id="higherPriority"
+				name="higherPriority"
+				type="checkbox"
+				bind:checked={newItemHigerPriority}
+			/>Higher Priority
+		</label>
+		<button class="btn btn-primary">Save Item</button>
+	</form>
+{/if}
+
+{#if items.length > 0}
+	<ul>
+		{#each items as item (item.id)}
+			<li>{item.id} - {item.label}</li>
+		{/each}
+	</ul>
+{:else}
+	<p>Nothing to see here</p>
+{/if}
